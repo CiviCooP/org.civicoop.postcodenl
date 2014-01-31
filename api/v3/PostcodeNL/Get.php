82c4d7b6ff0de5ff085a8d54259ca0f089b475d9
Jaap Jansma <info@edeveloper.nl>
2014-01-31 12:05:05 +0100

    added an api for retrieving postcodes

---
+++ api/v3/PostcodeNL/Get.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * PostcodeNL.Get API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+/*function _civicrm_api3_postcode_n_l_get_spec(&$spec) {
+  $spec['magicword']['api.required'] = 1;
+}*/
+
+/**
+ * PostcodeNL.Get API
+ * 
+ * Returns the found postcode, woonplaats, gemeente with the queried paramaters
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_postcode_n_l_get($params) {
+  $validParamFields = array(
+    'id',
+    'postcode',
+    'huisnummer',
+    'adres',
+    'woonplaats',
+    'gemeente',
+  );
+  $returnFields = array(
+    'id',
+    'postcode_nr',
+    'postcode_letter',
+    'huisnummer_van',
+    'huisnummer_tot',
+    'adres',
+    'even',
+    'provincie',
+    'gemeente',
+    'wijk',
+    'buurt'
+  );
+  
+  /* 
+   * check if at least one parameter is valid 
+   * Also break up an postcode into postcode number (4 digits) and postcode letter (2 letters).
+   *
+   */
+  $validatedParams = array();
+  foreach($params as $key => $value) {
+    if (in_array($key, $validParamFields)) {
+      if ($key == 'postcode') {
+        //extra validation is needed to break the postcode up into 4 digits and 2 letters
+        $postcode = preg_replace('/[^\da-z]/i', '', $value);
+        $postcode_4pp = substr($postcode, 0, 4); //select the four digist
+        $postcode_2pp = substr($postcode, 4, 2); //select the 2 letters
+        if (strlen($postcode_4pp) == 4) {
+          $validatedParams['postcode_nr'] = $postcode_4pp;
+        }
+        if (strlen($postcode_2pp) == 2) {
+          $validatedParams['postcode_letter'] = strtoupper($postcode_2pp);
+        }
+      } else {
+        $validatedParams[$key] = $value;
+      }
+    }
+  }
+  
+  $sql = "SELECT * FROM `civicrm_postcodenl` WHERE 1";
+  
+  /**
+   * Build the where clausule of the postcode
+   */
+  $where = "";
+  $values = array();
+  $i = 1;
+  foreach($validatedParams as $field => $value) {
+    if ($field == 'huisnummer') {
+      //huisnummer needs an between huisnummer_van and huisnummer_tot
+      //also there needs to be a check on even or odd
+      $even = ($value % 2 == 0 ? 1 : 0);
+      $where .= "AND `even` = %".$i;
+      $values[$i] = array($even, 'int');
+      $i++;
+      
+      $where .= " AND %".$i." BETWEEN `huisnummer_van` AND `huisnummer_tot`";
+      $values[$i] = array($value, 'int');
+      $i++;      
+    } else {
+      $where .= " AND `".$field."` = %".$i;
+      $values[$i] = array($value, 'String');
+      $i++;
+    }    
+  }
+  $sql .= $where . " LIMIT 0, 25";
+  $dao = CRM_Core_DAO::executeQuery($sql, $values);
+  
+  $returnValues = array();
+  while($dao->fetch()) {
+    var_dump($dao->_resultFields); exit();
+    $returnValues[$dao->id] = $dao;
+  }
+  
+  return civicrm_api3_create_success($returnValues, $params, 'PostcodeNL', 'get');
+}
+
--- postcodenl.php
+++ postcodenl.php
@@ -29,18 +29,19 @@ function postcodenl_civicrm_xmlMenu(&$files) {
  */
 function postcodenl_civicrm_install() {
   $sql = "
-  CREATE TABLE IF NOT EXISTS `postcodenl` (
+  CREATE TABLE IF NOT EXISTS `civicrm_postcodenl` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `postcode_nr` int(4) NOT NULL,
   `postcode_letter` varchar(2) NOT NULL,
   `huisnummer_van` int(11) NOT NULL,
   `huisnummer_tot` int(11) NOT NULL,
+  `adres` varchar(255) NOT NULL,
   `even` tinyint(1) NOT NULL DEFAULT '1',
   `provincie` varchar(255) NOT NULL,
   `gemeente` varchar(255) NOT NULL,
   `woonplaats` varchar(255) NOT NULL,
-  `Wijk` varchar(255) NOT NULL,
-  `Buurt` varchar(255) NOT NULL,
+  `wijk` varchar(255) NOT NULL,
+  `buurt` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
   CRM_Core_DAO::executeQuery($sql);
@@ -53,7 +54,7 @@ function postcodenl_civicrm_install() {
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
 function postcodenl_civicrm_uninstall() {
-  $sql = "DROP TABLE IF EXISTIS `postcodenl`";
+  $sql = "DROP TABLE IF EXISTIS `civicrm_postcodenl`";
   CRM_Core_DAO::executeQuery($sql);
   return _postcodenl_civix_civicrm_uninstall();
 }
