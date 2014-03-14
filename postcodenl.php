1779285f65ef168d7b982cb161b9c5a108c1da93
Jaap Jansma <info@edeveloper.nl>
2014-03-14 09:42:06 +0100

    added upgrader class

--- postcodenl.php
+++ postcodenl.php
@@ -28,23 +28,6 @@ function postcodenl_civicrm_xmlMenu(&$files) {
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function postcodenl_civicrm_install() {
-  $sql = "
-  CREATE TABLE IF NOT EXISTS `civicrm_postcodenl` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `postcode_nr` int(4) NOT NULL,
-  `postcode_letter` varchar(2) NOT NULL,
-  `huisnummer_van` int(11) NOT NULL,
-  `huisnummer_tot` int(11) NOT NULL,
-  `adres` varchar(255) NOT NULL,
-  `even` tinyint(1) NOT NULL DEFAULT '1',
-  `provincie` varchar(255) NOT NULL,
-  `gemeente` varchar(255) NOT NULL,
-  `woonplaats` varchar(255) NOT NULL,
-  `wijk` varchar(255) NOT NULL,
-  `buurt` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-  CRM_Core_DAO::executeQuery($sql);
   return _postcodenl_civix_civicrm_install();
 }
 
@@ -54,8 +37,6 @@ function postcodenl_civicrm_install() {
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
 function postcodenl_civicrm_uninstall() {
-  $sql = "DROP TABLE IF EXISTIS `civicrm_postcodenl`";
-  CRM_Core_DAO::executeQuery($sql);
   return _postcodenl_civix_civicrm_uninstall();
 }
 
---
+++ sql/uninstall.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `civicrm_postcodenl`;
+DROP TABLE IF EXISTS `civicrm_pro6pp_import`;
